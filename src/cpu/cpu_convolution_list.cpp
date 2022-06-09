b38a5befd648bcded35b10cadf53d3018ceb5676
Luo Cheng <cheng.luo@intel.com>
2022-06-09 15:23:11 +0800

    remove unused primitive (#127)

--- src/cpu/cpu_convolution_list.cpp
+++ src/cpu/cpu_convolution_list.cpp
@@ -204,7 +204,7 @@ const std::map<pk_dt_impl_key_t, std::vector<impl_list_item_t>> &impl_list_map()
             nullptr,
         }},
         // BWD_D fp
-        {{backward_data, f32, f32, f32}, REG_BWD_D_PK({
+        {{backward_data, f32, f32, f32}, REG_CONV_P({
             CPU_INSTANCE_X64(ip_convolution_bwd_data_t)
             CPU_INSTANCE_AMX(brgemm_convolution_bwd_t<avx512_core_amx>)
             CPU_INSTANCE_AVX512(brgemm_convolution_bwd_t<avx512_core>)
@@ -224,7 +224,7 @@ const std::map<pk_dt_impl_key_t, std::vector<impl_list_item_t>> &impl_list_map()
             CPU_INSTANCE(ref_convolution_bwd_data_t)
             nullptr,
         })},
-        {{backward_data, f32, bf16, bf16}, REG_BWD_D_PK({
+        {{backward_data, f32, bf16, bf16}, REG_CONV_P({
             CPU_INSTANCE_X64(ip_convolution_bwd_data_t)
             CPU_INSTANCE_AMX(brgemm_convolution_bwd_t<avx512_core_amx>)
             CPU_INSTANCE_AMX(brgemm_convolution_bwd_strided_t<avx512_core_amx>)
@@ -239,7 +239,7 @@ const std::map<pk_dt_impl_key_t, std::vector<impl_list_item_t>> &impl_list_map()
             CPU_INSTANCE(ref_convolution_bwd_data_t)
             nullptr,
         })},
-        {{backward_data, bf16, bf16, bf16}, REG_BWD_D_PK({
+        {{backward_data, bf16, bf16, bf16}, REG_CONV_P({
             CPU_INSTANCE_X64(ip_convolution_bwd_data_t)
             CPU_INSTANCE_AMX(brgemm_convolution_bwd_t<avx512_core_amx>)
             CPU_INSTANCE_AMX(brgemm_convolution_bwd_strided_t<avx512_core_amx>)
